e8f6e2dd632561187462d1e8c9dcaf1886456480
wu1024 <1141799512@qq.com>
2019-10-24 20:16:42 +0800

    work8

--- work8/glodbach.js
+++ work8/glodbach.js
@@ -1,23 +1,31 @@
-var num = parseInt(prompt("输入一个偶数"));
-isTrue(num);
-function factorial(a){
-    var sum=0;
-    for(var i = 1; i<=a; i++){
-        if(a % i ==0){
-            sum++;
-        }
+function goldbach() {
+        var value = document.getElementById('odd').value
+        if(value%2!==0){
+            alert("请输入偶数")
+        }
+        let goldbach = document.getElementById('goldbach')
+        var arr =[]
+        var a = 0;
+        for(var i=2;i<=value;i++){
+            a = 0
+            for(var j=2;j<i;j++){
+                if(i%j==0){
+                    a++
+                }
+            }
+            if(a==0){
+                arr.push(i)
+            }
+        }
+        var str = ''
+        for(let i=0;i<(arr.length)/2;i++){
+           for(let j=0;j<arr.length;j++){
+               if((arr[i]+arr[j])===Number(value)){
+                   str += '<div>'+value+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和"+ '</div>'
+               }
+           }
+        }
+        goldbach.innerHTML = str
     }
-    if(sum==2){
-        return true;
-    }else{
-        return false;
-    }
-}
-function isTrue(a){
-    for(var i=2;i<a-2;i++){
-        var j = a - i;
-        if(factorial(i)&&factorial(j)){
-            console.log(a+"可以拆分为两个质数"+ i+"与"+j+"的和");
-        }
-    }
-}
+    
+    
