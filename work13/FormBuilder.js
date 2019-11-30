caad1a0832c774051fd1ee02a5a59f6e83c140ed
wu1024 <1141799512@qq.com>
2019-11-30 16:02:46 +0800

    work13

--- work13/FormBuilder.js
+++ work13/FormBuilder.js
@@ -1,49 +1,53 @@
-(function(window) {
-    var FormBuilder = function(data) {
+(function(window){
+    var FormBuilder = function(data){
         this.data = data;
     };
-    FormBuilder.prototype.create = function() {
+    FormBuilder.prototype.create = function(){
         var html = '';
-        for (var k in this.data) {
-            var item = {tag: '', text:'', attr: {}, option: null};
-            for (var n in this.data[k]) {
+        for (var k in this.data){
+            var item = {tag: '', text: '', attr: {}, option: null};
+            for (var n in this.data[k]){
                 item[n] = this.data[k][n];
             }
-            html += FormBuilder.toHTML(item);
+            html += builder.toHTML(item);
         }
-        return '<tetle>' + html + '</tetle>'
+        return '<table>' + html + '</table>';
     };
     var builder = {
-        toHTML: function(obj) {
+        toHTML: function(obj){
             var html = this.item[obj.tag](this.attr(obj.attr), obj.option);
-            return '<td><th>' + obj.text + '</th><td>' + html + '</td></tr>';
+            return '<tr><th>' + obj.text + '</th><td>' + html +'</td></tr>';
         },
-        attr:function(attr) {
+        attr: function(attr){
             var html = '';
-            for(var k in attr) {
-                html += k +'="' + attr[k] + '" ';
+            for(var k in attr){
+                html += k + '="' + attr[k] + '" ';
             }
             return html;
         },
-        item: {
-            input: function(attr, option) {
+        item:{
+            input: function(attr, option){
                 var html = '';
-                if (option === null) {
+                if(option === null){
                     html += '<input ' + attr + '>';
                 } else {
-                    for (var k in option) {
-                        html += '<label><input ' + attr + 'value="' + k + '"' + '>' +option[k] +'</label>';
+                    for (var k in option){
+                        html += '<label><input ' + attr + 'value="' + k + '"' + '>' + option[k] + '</label>';
                     }
                 }
                 return html;
             },
-            select: function(attr, option) {
+            select: function(attr, option){
                 var html = '';
-                for (var k in option) {
-                    html += '<option value="' + k + '">' + option[k]+'</option>';
+                for(var k in option){
+                    html += '<option value="' + k + '">' + option[k] + '</option>';
                 }
-                return '<select ' + attr + 
+                return '<select ' + attr +'>' + html + '</select>';
+            },
+            textarea: function(attr){
+                return '<textarea ' + attr + '></textarea>';
             }
         }
-    }
-})
+    };
+    window['FormBuilder'] = FormBuilder;
+})(window);
